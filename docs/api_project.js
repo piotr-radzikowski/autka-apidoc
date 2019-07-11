5ca9c39ab0a31ee0b4525e4a4291ec8cd4115f9e
Radzikowski Piotr <piotr.radzikowski@mfind.pl>
2019-07-11 10:32:28 +0200

    fix docs path

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-07-11T08:23:34.017Z",
+    "time": "2019-07-11T08:31:12.831Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
